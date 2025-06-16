3f36f6c516fe7c5fe72e55ed0e991fcb0f50813a
Kaveen Thilakarathna <kaveenthilakarathna582@gmail.com>
2025-06-16 12:38:39 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,13 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full" suppressHydrationWarning>
+    <html lang="en" className="h-full">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased h-full">
+      <body className="font-body antialiased h-full" suppressHydrationWarning={true}>
         <AppThemeProvider
           attribute="class"
           defaultTheme="system"
@@ -38,3 +38,4 @@ export default function RootLayout({
     </html>
   );
 }
+
