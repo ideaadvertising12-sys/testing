d1b5f73b7f7f34f14dc804d17db649b4a3d57d29
Kaveen Thilakarathna <kaveenthilakarathna582@gmail.com>
2025-09-25 10:39:19 +0000

    Add customer search function

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@
 import { GlobalPreloaderScreen } from "@/components/GlobalPreloaderScreen";
 
 export default function Loading() {
-  // This loading UI will be shown while the root page (e.g., login page) is loading its content.
+  //// This loading UI will be shown while the root page (e.g., login page) is loading its content.
   return (
     <>
       <GlobalPreloaderScreen message="Loading application..." />
